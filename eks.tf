a0697229df8673091b3dd54142ec41b989c39f48
Rajesh Radhakrishnan <rajeshr_2020@outlook.com>
2023-08-28 01:48:56 +0530

    cluster create

---
+++ eks.tf
@@ -0,0 +1,13 @@
+# Create EKS Cluster
+resource "aws_eks_cluster" "Learning-eks" {
+ name = "Learning-cluster"
+ role_arn = aws_iam_role.eks-iam-role.arn
+
+ vpc_config {
+  subnet_ids = [var.subnet_id_1, var.subnet_id_2]
+ }
+
+ depends_on = [
+  aws_iam_role.eks-iam-role,
+ ]
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+ variable "subnet_id_1" {
+  type = string
+  default = "subnet-0eb1ecc0f95f88425"
+ }
+ 
+ variable "subnet_id_2" {
+  type = string
+  default = "subnet-08da88830e6a5822c"
+ }
